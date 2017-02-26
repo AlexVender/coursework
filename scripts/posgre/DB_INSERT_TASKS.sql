7ae4ebb146b21e34bc8a82adf757748d0bb31036
Оля Моссоулина <chanta595@gmail.com>
2017-02-26 18:05:46 +0400

    created script of insert

---
+++ scripts/posgre/DB_INSERT_TASKS.sql
@@ -0,0 +1,10 @@
+INSERT INTO tasks (name, description, priority, status, start_date, due_date, assignee_id, project_id) VALUES
+  ('hello world','get acquainted with employees of the company',2,0,'2017-02-26','2017-02-27',1,3),
+  ('hello world','get acquainted with employees of the company',2,2,'2017-02-24','2017-02-28',2,2),
+  ('hello world','get acquainted with employees of the company',2,3,'2017-02-22','2017-02-26',3,4),
+  ('hello world','get acquainted with employees of the company',2,4,'2017-02-21','2017-02-26',5,5),
+  ('execute order 1','execute order 1: create menu',2,4,'2017-02-26','2017-02-27',7,6),
+  ('coffee','make coffee',0,0,'2017-02-26','2017-02-26',4,2),
+  ('business plan','write a business plan',2,2,'2017-02-25','2017-02-27',7,1),
+  ('meeting','organize a banquet',2,1,'2017-03-1','2017-03-7',8,1),
+  ('invitation','invite guests',2,4,'2017-02-26','2017-03-6',9,1)
